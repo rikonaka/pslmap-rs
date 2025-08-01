7ca774e4f80fb55a50cdd4dde9d89c744d3e3a07
Riko <xxy1836@gmail.com>
2025-08-01 15:31:08 +0800

    2025-8-1 works

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
+use chrono::DateTime;
+use chrono::Local;
 use clap::ArgAction;
 use clap::Parser;
 use pistol::PistolLogger;
 use pistol::PistolRunner;
 use pistol::Target;
 use pistol::icmp_ping;
+use pistol::mac_scan;
+use pistol::ping::PingStatus;
+use std::collections::BTreeMap;
+use std::fmt;
 use std::fs::File;
 use std::io::BufRead;
 use std::io::BufReader;
@@ -14,6 +20,48 @@ use std::sync::Arc;
 use std::sync::LazyLock;
 use std::sync::Mutex;
 use std::time::Duration;
+use std::time::Instant;
+
+/// Nmap rust version.
+#[derive(Parser, Debug)]
+#[command(author = "RikoNaka", version, about, long_about = None)]
+struct Args {
+    /// Input target
+    #[arg(short, long, default_value = "")]
+    target: String,
+
+    /// Input from list of hosts/networks (same as nmap -iL option)
+    #[arg(short, long, default_value = "")]
+    filename: String,
+
+    /// Specified ports
+    #[arg(short, long, default_value = "")]
+    ports: String,
+
+    /// Ping Scan - host discovery
+    #[arg(long, action(ArgAction::SetTrue), default_value_t = false)]
+    pingscan: bool,
+
+    /// Mac Scan - host discovery
+    #[arg(long, action(ArgAction::SetTrue), default_value_t = false)]
+    macscan: bool,
+
+    /// Timeout
+    #[arg(long, default_value_t = 1.0)]
+    timeout: f64,
+
+    /// Set the IPv6 address to have the highest priority (this means that when the target is a domain name, the program will first use the IPv6 address as the target address)
+    #[arg(long, action(ArgAction::SetTrue), default_value_t = false)]
+    ipv6: bool,
+
+    /// The udp listen port
+    #[arg(short, long, default_value = "")]
+    udp: String,
+
+    /// When receiving data, return the data set in this parameter
+    #[arg(short, long, default_value = "null", default_missing_value = "", num_args(0..2))]
+    need_return: String,
+}
 
 static IPV6_FIRST: LazyLock<Arc<Mutex<bool>>> = LazyLock::new(|| Arc::new(Mutex::new(false)));
 
@@ -161,11 +209,54 @@ fn target_from_input(target_addr: &str, target_ports: &str) -> Vec<Target> {
     target_parser(target_addr, target_ports)
 }
 
-fn ping_scan(targets: &[Target], timeout: f64) {
+static REPO_LINK: &str = "https://github.com/rikonaka/pslmap-rs";
+
+struct InfoShow;
+
+impl InfoShow {
+    fn print(info: &str, tail: &str) {
+        let app = env!("CARGO_PKG_NAME");
+        let version = env!("CARGO_PKG_VERSION");
+        let now: DateTime<Local> = Local::now();
+        let formatted_time = now.format("%Y-%m-%d %H:%M %Z").to_string();
+        println!(
+            "Starting {} {} ({}) at {}",
+            app, version, REPO_LINK, formatted_time,
+        );
+        println!("{}", info);
+        println!("{}", tail);
+    }
+}
+
+/// Nmap Doc (https://nmap.org/book/man-host-discovery.html):
+/// The default host discovery done with -sn consists of an ICMP echo request,
+/// TCP SYN to port 443, TCP ACK to port 80, and an ICMP timestamp request by default.
+/// When executed by an unprivileged user, only SYN packets are sent (using a connect call) to ports 80 and 443
+/// on the target. When a privileged user tries to scan targets on a local ethernet network,
+/// ARP requests are used unless --send-ip was specified.
+#[derive(Debug, Clone, Copy)]
+enum HostDiscoveryStatus {
+    Up,
+    Down,
+}
+
+impl fmt::Display for HostDiscoveryStatus {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let s = match self {
+            HostDiscoveryStatus::Up => "up",
+            HostDiscoveryStatus::Down => "down",
+        };
+        write!(f, "{}", s)
+    }
+}
+
+/// Same as the ping command in the system.
+fn host_discovery_ping_scan(targets: &[Target], timeout: f64) {
+    let start = Instant::now();
+
     let _pr =
         PistolRunner::init(PistolLogger::None, None, None).expect("init pistol runner failed");
 
-    println!("target len: {}", targets.len());
     let num_threads = None;
     let src_addr = None;
     let src_port = None;
@@ -180,44 +271,87 @@ fn ping_scan(targets: &[Target], timeout: f64) {
         max_attempts,
     )
     .expect("icmp ping failed");
-    println!("{}", ret);
+
+    // sorted
+    let mut btm = BTreeMap::new();
+    for ping in ret.ping_reports {
+        btm.insert(ping.addr, ping.clone());
+    }
+
+    let mut hosts_up = 0;
+    let mut info = Vec::new();
+    for (addr, ping) in btm {
+        let new_status = match ping.status {
+            PingStatus::Up => {
+                hosts_up += 1;
+                HostDiscoveryStatus::Up
+            }
+            _ => HostDiscoveryStatus::Down,
+        };
+        let line = format!(
+            "{} -> {} ({:.2}s)",
+            addr,
+            new_status,
+            ping.cost.as_secs_f64()
+        );
+        info.push(line);
+    }
+
+    let info = info.join("\n");
+    let tail = format!(
+        "Nmap done: {} IP addresses ({} hosts up) scanned in {:.2} seconds",
+        targets.len(),
+        hosts_up,
+        start.elapsed().as_secs_f64()
+    );
+    InfoShow::print(&info, &tail);
 }
 
-/// Nmap rust version.
-#[derive(Parser, Debug)]
-#[command(author = "RikoNaka", version, about, long_about = None)]
-struct Args {
-    /// Input target
-    #[arg(short, long, default_value = "")]
-    target: String,
+/// When the target address is local, you can use arp scan or ndp ns scan.
+fn host_discovery_mac_scan(targets: &[Target], timeout: f64) {
+    let start = Instant::now();
 
-    /// Input from list of hosts/networks (same as nmap -iL option)
-    #[arg(short, long, default_value = "")]
-    filename: String,
+    let _pr =
+        PistolRunner::init(PistolLogger::None, None, None).expect("init pistol runner failed");
 
-    /// Specified ports
-    #[arg(short, long, default_value = "")]
-    ports: String,
+    let num_threads = None;
+    let src_addr = None;
+    let max_attempts = 2;
+    let timeout = Some(Duration::from_secs_f64(timeout));
+    let ret = mac_scan(&targets, num_threads, src_addr, timeout, max_attempts).expect("mac failed");
 
-    /// Ping Scan - disable port scan (same as nmap -sn option)
-    #[arg(long, action(ArgAction::SetTrue), default_value_t = false)]
-    pingscan: bool,
+    // sorted
+    let mut all_ips = Vec::new();
+    for target in targets {
+        all_ips.push(target.addr);
+    }
+    let mut btm = BTreeMap::new();
+    for mr in ret.mac_reports {
+        btm.insert(mr.addr, mr.clone());
+    }
 
-    /// Timeout
-    #[arg(long, default_value_t = 1.0)]
-    timeout: f64,
+    let mut hosts_up = 0;
+    let mut info = Vec::new();
+    for (addr, mr) in btm {
+        let new_status = match mr.mac {
+            Some(mac) => {
+                hosts_up += 1;
+                format!("{}({})", HostDiscoveryStatus::Up, mac)
+            }
+            _ => format!("{}", HostDiscoveryStatus::Down),
+        };
+        let line = format!("{} -> {}", addr, new_status);
+        info.push(line);
+    }
 
-    /// Set the IPv6 address to have the highest priority (this means that when the target is a domain name, the program will first use the IPv6 address as the target address)
-    #[arg(long, action(ArgAction::SetTrue), default_value_t = false)]
-    ipv6: bool,
-
-    /// The udp listen port
-    #[arg(short, long, default_value = "")]
-    udp: String,
-
-    /// When receiving data, return the data set in this parameter
-    #[arg(short, long, default_value = "null", default_missing_value = "", num_args(0..2))]
-    need_return: String,
+    let info = info.join("\n");
+    let tail = format!(
+        "Nmap done: {} IP addresses ({} hosts up) scanned in {:.2} seconds",
+        targets.len(),
+        hosts_up,
+        start.elapsed().as_secs_f64()
+    );
+    InfoShow::print(&info, &tail);
 }
 
 fn main() {
@@ -244,6 +378,8 @@ fn main() {
     }
 
     if args.pingscan {
-        ping_scan(&targets, args.timeout);
+        host_discovery_ping_scan(&targets, args.timeout);
+    } else if args.macscan {
+        host_discovery_mac_scan(&targets, args.timeout);
     }
 }
